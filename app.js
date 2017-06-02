63a632f15a7bfcf1a433d943260957addc9c7fa4
Bryant Huang <b.luv.sic@gmail.com>
2017-06-02 15:08:24 -0700

    working implementation

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// TODO
+class GroceryListItem extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      done : false
+    };
+  }
+  onListItemHover(event) {
+    this.setState({
+      done : !this.state.done
+    })
+  }
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+    return (
+      <li style={style} onMouseEnter={this.onListItemHover.bind(this)} onMouseLeave={this.onListItemHover.bind(this)}>{this.props.item}</li>
+    )
+  }
+}
+
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item =>
+      <GroceryListItem item={item}/>
+    )}
+  </ul>
+)
+//
+// var GroceryList = () => (
+//   <GroceryListItem items={['Kale', 'Cucumbers']}/>
+// )
+
+ReactDOM.render(<GroceryList items={['Kale', 'Cucumbers', 'chickcen ', 'currey']}/>, document.getElementById('app'))
